e20ef43696a5578460d0e9bbcbc02329dc61cb53
Maksim Perepelitsa <chimax.prog@gmail.com>
2018-03-20 19:27:47 +0300

    implement Direction

---
+++ src/Direction/Direction.java
@@ -0,0 +1,13 @@
+package Direction;
+
+import java.util.Scanner;
+
+class Direction {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int position = scanner.nextInt();
+        final String[] actions = {"do not move", "move up", "move down", "move left", "move right"};
+        System.out.println((position >= 0 && position < 5) ? actions[position] : "error!");
+    }
+}
