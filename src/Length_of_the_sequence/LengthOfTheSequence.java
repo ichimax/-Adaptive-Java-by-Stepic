f8b534f03c04f4242fb4352f2ee26c7837dd4589
Maksim Perepelitsa <chimax.prog@gmail.com>
2018-03-24 23:29:49 +0300

    implement Length of the sequence

---
+++ src/Length_of_the_sequence/LengthOfTheSequence.java
@@ -0,0 +1,32 @@
+package Length_of_the_sequence;
+
+import java.util.Scanner;
+
+/*On the input, the program gets a sequence of non-negative integers; each integer is written in a separate line. The sequence ends with an integer 0, when reading which the program should end its work and output the length of the sequence (not counting the final 0).
+
+Don’t read numbers following the number 0.
+
+Sample Input:
+
+1
+7
+9
+0
+5
+Sample Output:
+
+3*/
+
+public class LengthOfTheSequence {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        while(scanner.nextInt() != 0) {
+            counter++;
+        }
+
+        System.out.println(counter);
+    }
+}
