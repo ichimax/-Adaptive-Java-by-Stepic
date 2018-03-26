b5ae0c0df9b4ea5cde0d12da911364694a7e5cdd
Maksim Perepelitsa <chimax.prog@gmail.com>
2018-03-26 17:53:01 +0300

    add the task

--- src/Interval/Interval.java
+++ src/Interval/Interval.java
@@ -2,6 +2,8 @@ package Interval;
 
 import java.util.Scanner;
 
+/*IntervalGiven an integer as input. Output True if its value is within the interval (−15,12]∪(14,17)∪[19,+∞), and False otherwise (case sensitive).Please note the different brackets, which are used to specify intervals. The problem uses semi-open and open intervals. You can read more about it on the Wikipedia.*/
+
 public class Interval {
 
     public static void main(String[] args) {
