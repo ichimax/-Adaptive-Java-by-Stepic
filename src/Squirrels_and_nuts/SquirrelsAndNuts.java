726f1a61cf58b5d24f954dac20091991f05306f3
Maksim Perepelitsa <chimax.prog@gmail.com>
2018-03-26 20:52:39 +0300

    implement Squirrels and nuts

---
+++ src/Squirrels_and_nuts/SquirrelsAndNuts.java
@@ -0,0 +1,29 @@
+package Squirrels_and_nuts;
+
+import java.util.Scanner;
+
+/*Squirrels and nuts - 1
+
+N squirrels found K nuts and decided to divide them equally. Determine how many nuts each squirrel will get.
+
+Input data format
+
+There are two positive numbers N and K, each of them is not greater than 10000.
+
+Sample Input:
+
+3
+14
+Sample Output:
+
+4*/
+
+public class SquirrelsAndNuts {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int squirrels = scanner.nextInt();
+        System.out.print(scanner.nextInt() / squirrels);
+    }
+}
